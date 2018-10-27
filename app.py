66eeb552e29a01cd17da53f33cecef1d1ca325a9
vlarie <33710641+vlarie@users.noreply.github.com>
2018-10-27 14:40:29 -0500

    Updates to dependencies for Heroku deployment, added config.py

--- app.py
+++ app.py
@@ -5,19 +5,22 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
+import config.py
 
 app = Flask(__name__)
 
 # Establish MongoDB connection
-app.config["MONGO_URI"] = "mongodb://<dbuser>:<dbpassword>@ds111063.mlab.com:11063/heroku_4s5hf8p0"
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+# This configuration is for Heroku and is in development
+app.config["MONGO_URI"] = f"mongodb://<{dbuser}>:<{dbpassword}>@ds111063.mlab.com:11063/heroku_4s5hf8p0"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def home(): 
     print("Server received request for 'Home' page")
-    marsData = scrape_mars.sample_data
-    # marsData = mongo.db.mars.find_one()
+    # marsData = scrape_mars.sample_data
+    marsData = mongo.db.mars.find_one()
     return render_template("index.html", mars=marsData)
 
 @app.route("/scrape")
